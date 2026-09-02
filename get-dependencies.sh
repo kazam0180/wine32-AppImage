ce50fc8328eb4f3aff63b93992125dd47ba69e57
Mechamorph <kazam0180@proton.me>
2026-09-02 13:02:36 +0500

    Minimal 32-bit CLI Wine AppImage
    - Enable multilib repo for lib32-glibc
    - Strip all GUI deployments (SDL, Vulkan, OpenGL, GStreamer, PipeWire)
    - Remove only essential wine binaries (wine, wineserver, wineboot, regsvr32)
    - Remove 64-bit ELF files and x86_64 wine directories after deployment
    - Keep patchelf + linker hook for AppImage compatibility

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,19 +4,17 @@ set -eu
 
 ARCH=$(uname -m)
 
+echo "Enabling multilib repo..."
+echo "---------------------------------------------------------------"
+sudo sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf
+sudo pacman -Syu --noconfirm
+
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm wine cabextract sdl2 pipewire-audio pipewire-jack harfbuzz gst-plugins-bad gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly gst-libav gstreamer 7zip unzip
-
-echo "Installing debloated packages..."
-echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+pacman -Syu --noconfirm wine lib32-glibc
 
 if [ "$ARCH" = 'x86_64' ]; then
 	sudo pacman -S --noconfirm mingw-w64-binutils
 fi
 
-# Comment this out if you need an AUR package
-make-aur-package zenity-rs-bin
-
-# If the application needs to be manually built that has to be done down here
+echo "Done."
--- make-appimage.sh
+++ make-appimage.sh
@@ -1,56 +1,36 @@
 #!/bin/sh
 
+# Minimal 32-bit CLI-only Wine AppImage
+# For running CLI apps like COD1 map compiler (coutils.exe)
+# Uses Wine WoW64 - 64-bit wine binary runs 32-bit Windows apps
+
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q wine | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q wine | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/bcf6aa9582f676e1c93d0022319e6055cd1f2de2/Papirus/64x64/apps/wine.svg
 export DESKTOP=/usr/share/applications/wine.desktop
-export APPNAME=wine
-export DEPLOY_SDL=1
-export DEPLOY_PIPEWIRE=1
-export DEPLOY_GSTREAMER=1
-export DEPLOY_VULKAN=1
-export DEPLOY_OPENGL=1
+export APPNAME=wine32-cli
 
-# Deploy dependencies
+# Disable all GUI-related deployments for CLI-only mode
+export DEPLOY_SDL=0
+export DEPLOY_PIPEWIRE=0
+export DEPLOY_GSTREAMER=0
+export DEPLOY_VULKAN=0
+export DEPLOY_OPENGL=0
+
+# Deploy only essential wine binaries and libs
 mkdir -p /tmp/wine
 WINEPREFIX=/tmp/wine quick-sharun \
-	/usr/bin/wine*             \
-	/usr/lib/wine              \
-	/usr/bin/msidb             \
-	/usr/bin/msiexec           \
-	/usr/bin/notepad           \
-	/usr/bin/regedit           \
+	/usr/bin/wine              \
+	/usr/bin/wineserver        \
+	/usr/bin/wineboot          \
 	/usr/bin/regsvr32          \
-	/usr/bin/widl              \
-	/usr/bin/wmc               \
-	/usr/bin/wrc               \
-	/usr/bin/function_grep.pl  \
-	/usr/bin/cabextract        \
-	/usr/lib/libfreetype.so*   \
-	/usr/lib/libharfbuzz*      \
-	/usr/lib/libgraphite*      \
-	/usr/lib/libavcodec.so*	   \
-	/usr/lib/libavdevice.so*   \
-	/usr/lib/libavfilter.so*   \
-	/usr/lib/libavformat.so*   \
-	/usr/lib/libavutil.so*     \
-	/usr/lib/libswresample.so* \
-	/usr/lib/libswscale.so*    \
-	/usr/bin/wget              \
-	/usr/bin/zenity            \
-	/usr/bin/unzip             \
-	/usr/lib/7zip/7z           \
-	/usr/lib/7zip/7z.so
-
-# Install latest winetricks
-wget --retry-connrefused --tries=30 https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O ./AppDir/bin/winetricks
-chmod +x ./AppDir/bin/winetricks
+	/usr/lib/wine
 
 # alright here the pain starts
 ln -sr ./AppDir/lib/wine/x86_64-unix/*.so* ./AppDir/bin
@@ -60,10 +40,6 @@ kek=.$(tr -dc 'A-Za-z0-9_=-' < /dev/urandom | head -c 10)
 rm -f ./AppDir/lib/wine/x86_64-unix/wine
 cp /usr/lib/wine/x86_64-unix/wine ./AppDir/lib/wine/x86_64-unix/wine
 patchelf --set-interpreter /tmp/"$kek" ./AppDir/lib/wine/x86_64-unix/wine
-# we used to run patchelf --add-needed anylinux.so on the wine binary
-# but after 11.8 this causes the binary to break horribly:
-# AppDir/lib/wine/x86_64-unix/wine: oops... not enough space for load commands
-# so we will ahve to make sure anylinux.so loads by adding it as a dependency to the libc
 patchelf --add-needed anylinux.so ./AppDir/shared/lib/libc.so.6
 
 cat <<EOF > ./AppDir/bin/random-linker.src.hook
@@ -72,15 +48,18 @@ cp -f "\$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
 EOF
 chmod +x ./AppDir/bin/*.hook
 
-# Set the lib path to also use wine libs
-echo 'LD_LIBRARY_PATH=${APPDIR}/lib:${APPDIR}/lib/pulseaudio:${APPDIR}/lib/alsa-lib:${APPDIR}/lib/wine/x86_64-unix' >> ./AppDir/.env
+echo 'LD_LIBRARY_PATH=${APPDIR}/lib:${APPDIR}/lib/wine/x86_64-unix' >> ./AppDir/.env
 
-# strip windows libs, inspired by alpine linux: 
-# https://gitlab.alpinelinux.org/alpine/aports/-/blob/master/community/wine/APKBUILD
+# Strip Windows DLLs
 if [ "$ARCH" = 'x86_64' ]; then
-	x86_64-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/x86_64-windows/*.dll
-	i686-w64-mingw32-strip   -R .comment --strip-unneeded ./AppDir/lib/wine/i386-windows/*.dll
+	i686-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/i386-windows/*.dll 2>/dev/null || true
 fi
+
+# Remove x86_64 Windows DLLs - only need i386 for 32-bit apps
+rm -rf ./AppDir/lib/wine/x86_64-windows
+
+# Clean up broken symlinks
+find ./AppDir/ -xtype l -delete 2>/dev/null || true
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
