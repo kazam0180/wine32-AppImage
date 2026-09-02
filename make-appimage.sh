347d797d495b008659cadf74dc1350da2b6d1cb4
Mechamorph <kazam0180@proton.me>
2026-09-02 07:59:22 +0500

    init

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,85 +1,46 @@
 #!/bin/sh
 
+# Minimal CLI-only Wine AppImage
+# For running CLI apps like COD1 map compiler (coutils.exe)
+
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q wine | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q wine | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/bcf6aa9582f676e1c93d0022319e6055cd1f2de2/Papirus/64x64/apps/wine.svg
 export DESKTOP=/usr/share/applications/wine.desktop
-export APPNAME=wine
-export DEPLOY_SDL=1
-export DEPLOY_PIPEWIRE=1
-export DEPLOY_GSTREAMER=1
-export DEPLOY_VULKAN=1
-export DEPLOY_OPENGL=1
+export APPNAME=wine-cli
 
-# Deploy dependencies
+# Disable all GUI-related deployments for CLI-only mode
+export DEPLOY_SDL=0
+export DEPLOY_PIPEWIRE=0
+export DEPLOY_GSTREAMER=0
+export DEPLOY_VULKAN=0
+export DEPLOY_OPENGL=0
+export DEPLOY_QT=0
+export DEPLOY_GTK=0
+export DEPLOY_PULSE=0
+
+# Deploy only minimal CLI Wine dependencies
+# Wine 11.x layout: usr/lib/wine/{i386-windows,x86_64-windows,x86_64-unix}
 mkdir -p /tmp/wine
 WINEPREFIX=/tmp/wine quick-sharun \
-	/usr/bin/wine*             \
+	/usr/bin/wine              \
+	/usr/bin/wineserver        \
+	/usr/bin/wineboot          \
 	/usr/lib/wine              \
-	/usr/bin/msidb             \
-	/usr/bin/msiexec           \
-	/usr/bin/notepad           \
-	/usr/bin/regedit           \
-	/usr/bin/regsvr32          \
-	/usr/bin/widl              \
-	/usr/bin/wmc               \
-	/usr/bin/wrc               \
-	/usr/bin/function_grep.pl  \
-	/usr/bin/cabextract        \
-	/usr/lib/libfreetype.so*   \
-	/usr/lib/libharfbuzz*      \
-	/usr/lib/libgraphite*      \
-	/usr/lib/libavcodec.so*	   \
-	/usr/lib/libavdevice.so*   \
-	/usr/lib/libavfilter.so*   \
-	/usr/lib/libavformat.so*   \
-	/usr/lib/libavutil.so*     \
-	/usr/lib/libswresample.so* \
-	/usr/lib/libswscale.so*    \
-	/usr/bin/wget              \
-	/usr/bin/zenity            \
-	/usr/bin/unzip             \
-	/usr/lib/7zip/7z           \
-	/usr/lib/7zip/7z.so
+	/usr/bin/regsvr32
 
-# Install latest winetricks
-wget --retry-connrefused --tries=30 https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks -O ./AppDir/bin/winetricks
-chmod +x ./AppDir/bin/winetricks
-
-# alright here the pain starts
-ln -sr ./AppDir/lib/wine/x86_64-unix/*.so* ./AppDir/bin
-
-# this gets broken by sharun somehow
-kek=.$(tr -dc 'A-Za-z0-9_=-' < /dev/urandom | head -c 10)
-rm -f ./AppDir/lib/wine/x86_64-unix/wine
-cp /usr/lib/wine/x86_64-unix/wine ./AppDir/lib/wine/x86_64-unix/wine
-patchelf --set-interpreter /tmp/"$kek" ./AppDir/lib/wine/x86_64-unix/wine
-# we used to run patchelf --add-needed anylinux.so on the wine binary
-# but after 11.8 this causes the binary to break horribly:
-# AppDir/lib/wine/x86_64-unix/wine: oops... not enough space for load commands
-# so we will ahve to make sure anylinux.so loads by adding it as a dependency to the libc
-patchelf --add-needed anylinux.so ./AppDir/shared/lib/libc.so.6
-
-cat <<EOF > ./AppDir/bin/random-linker.src.hook
-#!/bin/sh
-cp -f "\$APPDIR"/shared/lib/ld-linux*.so* /tmp/"$kek"
-EOF
-chmod +x ./AppDir/bin/*.hook
-
-# Set the lib path to also use wine libs
-echo 'LD_LIBRARY_PATH=${APPDIR}/lib:${APPDIR}/lib/pulseaudio:${APPDIR}/lib/alsa-lib:${APPDIR}/lib/wine/x86_64-unix' >> ./AppDir/.env
-
-# strip windows libs, inspired by alpine linux: 
-# https://gitlab.alpinelinux.org/alpine/aports/-/blob/master/community/wine/APKBUILD
-if [ "$ARCH" = 'x86_64' ]; then
-	x86_64-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/x86_64-windows/*.dll
-	i686-w64-mingw32-strip   -R .comment --strip-unneeded ./AppDir/lib/wine/i386-windows/*.dll
+# Strip Windows DLLs to reduce size
+if command -v x86_64-w64-mingw32-strip >/dev/null 2>&1; then
+	x86_64-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/x86_64-windows/*.dll 2>/dev/null || true
+fi
+if command -v i686-w64-mingw32-strip >/dev/null 2>&1; then
+	i686-w64-mingw32-strip -R .comment --strip-unneeded ./AppDir/lib/wine/i386-windows/*.dll 2>/dev/null || true
 fi
 
 # Turn AppDir into AppImage
