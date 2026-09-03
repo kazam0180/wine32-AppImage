9ea5bb362e38f948329f5ace02241f2ec1879f3b
Mechamorph <kazam0180@proton.me>
2026-09-03 14:34:59 +0500

    last try

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-sudo pacman -Syu --noconfirm patchelf
+sudo pacman -Syu --noconfirm cabextract lib32-pipewire-jack lib32-libpipewire lib32-pipewire lib32-zlib lib32-harfbuzz 7zip unzip fontconfig desktop-file-utils	lib32-fontconfig libxkbcommon lib32-libxcursor lib32-libxkbcommon lib32-libxi lib32-libxrandr lib32-wayland lib32-libunwind lib32-glib2	lib32-cairo  	lib32-pango	lib32-libogg lib32-libvorbis lib32-libxdamage lib32-libjpeg-turbo lib32-gtk3
 
 # Only install mingw strip tools for32-bit DLL stripping
 if [ "$ARCH" = 'x86_64' ]; then
@@ -29,5 +29,7 @@ mkdir -p /tmp/wine-x86
 tar -xf /tmp/wine-x86.tar.xz -C /tmp/wine-x86 --strip-components=1
 sudo cp -r /tmp/wine-x86/* /usr/
 rm -rf /tmp/wine*
+
+make-aur-package lib32-libcaca lib32-faac lib32-orc lib32-v4l-utils lib32-libtheora lib32-libvpx  lib32-mpg123 lib32-libcap lib32-libsoup3 lib32-twolame lib32-ffmpeg
 
 echo "Done."
